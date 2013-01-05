ec4485389d35650a2719d65b381a96691915bbc9
Gavin <halkeye@gmail.com>
2013-01-05 20:41:20 +0000

    inital commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+
+<head><meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title></title>
+	<meta name="description" content="converts xsl files to json">
+	<meta name="author" content="gavin@kodekoan.com">
+
+	<!--  Mobile viewport optimized: j.mp/bplateviewport -->
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+	<!-- CSS : implied media="all" -->
+	<link rel="stylesheet" href="css/style.css?v=1">
+
+</head>
+
+<body>
+
+  <a href="https://github.com/halkeye/xls2json"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub"></a>
+  <div id="container">
+    <div id="header">
+
+    </div>
+    
+    <div id="main">
+
+    </div>
+    
+    <div id="footer">
+
+    </div>
+  </div> <!--! end of #container -->
+
+  <!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet 
+       change the UA-XXXXX-X to be your site's ID -->
+  <script type="text/javascript">
+   var _gaq = [['_setAccount', 'UA-89920-9'], ['_trackPageview']];
+   (function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.async = true;
+    g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s);
+   })(document, 'script');
+  </script>
+  
+</body>
+</html>
